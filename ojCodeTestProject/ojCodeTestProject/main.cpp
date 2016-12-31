17511dd5cc6f3313ec01a42623401371eb1da505
songzc <914146142@qq.com>
2016-12-31 16:18:21 +0800

    git test

--- ojCodeTestProject/ojCodeTestProject/main.cpp
+++ ojCodeTestProject/ojCodeTestProject/main.cpp
@@ -1,10 +1,15 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
 
+	string str = "git is ok";
+	
 	cout << "oj project is created!" << endl;
+	cout << str << endl;
+
 	return 0;
 }
